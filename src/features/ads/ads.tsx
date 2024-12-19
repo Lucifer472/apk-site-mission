ef7105f42d52e016682a477cc8f8f69b39e6cee0
Lucifer472 <hardiksadhu472@gmail.com>
2024-12-19 18:11:51 +0530

    ads

--- src/features/ads/ads.tsx
+++ src/features/ads/ads.tsx
@@ -4,31 +4,31 @@ import useMedia from "use-media";
 import { AdsModule } from "./ads-module";
 
 export const TopAd = () => {
-  return <AdsModule adSlot="6301725753" />;
+  return <AdsModule adSlot="5927163452" />;
 };
 
 export const TopAdLarge = () => {
   const isLarge = useMedia({ minWidth: "768px" });
 
   if (!isLarge) return;
-  return <AdsModule adSlot="6301725753" />;
+  return <AdsModule adSlot="5927163452" />;
 };
 
 export const TopAdSmall = () => {
   const isSmall = useMedia({ maxWidth: "768px" });
 
   if (!isSmall) return;
-  return <AdsModule adSlot="6301725753" />;
+  return <AdsModule adSlot="5927163452" />;
 };
 
 export const MiddleAd = () => {
-  return <AdsModule adSlot="3379486197" />;
+  return <AdsModule adSlot="7023820376" />;
 };
 
 export const BottomMiddleAd = () => {
-  return <AdsModule adSlot="8348741840" />;
+  return <AdsModule adSlot="5239933590" />;
 };
 
 export const BottomAd = () => {
-  return <AdsModule adSlot="9093981333" />;
+  return <AdsModule adSlot="5735591762" />;
 };
